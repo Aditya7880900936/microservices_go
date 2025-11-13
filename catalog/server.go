29bc73738e53c0f8124690820a3636bf96ecd199
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-11-13 11:39:10 +0530

    feat(catalog): implement gRPC server and proto definitions
    - Add gRPC server implementation with basic service methods
    - Define proto file with Product messages and CatalogService
    - Include reflection for easier debugging

--- catalog/server.go
+++ catalog/server.go
@@ -1,1 +1,36 @@
 package catalog
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/Aditya7880900936/microservices_go/account/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type grpcServer struct {
+	pb.UnimplementedAccountServiceServer
+	service Service
+}
+
+func ListenGRPC(s Service, port int) error {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	serv := grpc.NewServer()
+	pb.RegisterAccountServiceServer(serv, &grpcServer{
+		UnimplementedAccountServiceServer: pb.UnimplementedAccountServiceServer{},
+		service:                           s, // ✅ add this parameter
+	})
+	reflection.Register(serv)
+	return serv.Serve(lis)
+}
+
+func (s *grpcServer) PostProduct(ctx context.Context)(){
+
+}
+
+ 
