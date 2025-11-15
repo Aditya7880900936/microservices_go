d043291d9ffa6a37b4e068f3c182963ff38711cc
Aditya7880900936 <adityasanskarsrivastav788@gmail.com>
2025-11-15 16:20:04 +0530

    feat(order): implement gRPC server and main application setup
    - Add gRPC server implementation with account and catalog service integration
    - Set up main application with configuration, repository initialization
    - Implement order posting functionality with validation and error handling
    - Add retry mechanism for database connection

--- order/cmd/order/main.go
+++ order/cmd/order/main.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/Aditya7880900936/microservices_go/order"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseURL string `envconfig:"DATABASE_URL"`
+	AccountURL  string `envconfig:"ACCOUNT_SERVICE_URL"`
+	CatalogURL  string `envconfig:"CATALOG_SERVICE_URL"`
+}
+
+func main() {
+	var cfg Config
+	err := envconfig.Process("", &cfg)
+	if err != nil {
+		panic(err)
+	}
+	var r order.Repository
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		r, err = order.NewPostgresRepository(cfg.DatabaseURL)
+		if err != nil {
+			return err
+		}
+		return
+	})
+	defer r.Close()
+	log.Println("Listening on port 8080...")
+	s := order.NewService(r)
+	log.Fatal(order.ListenGRPC(s, cfg.AccountURL, cfg.CatalogURL , 8080))
+}
